ba5286a1b17a0e6799aff94f3cdcf874702b47b1
Zaman Hüseynli <zamanhuseynli23@gmail.com>
2025-10-02 23:30:36 +0400

    Create c_build.rs

---
+++ gui-api/hdm_api/c_build.rs
@@ -0,0 +1,21 @@
+use std::fs;
+
+fn main() {
+    let cargo_manifest = "Cargo.toml";
+    let cargo_toml = fs::read_to_string(cargo_manifest).unwrap();
+
+    let mut new_toml = String::new();
+    for line in cargo_toml.lines() {
+        if line.trim_start().starts_with("crate-type") {
+            continue; // tamamen kaldır
+        }
+        new_toml.push_str(line);
+        new_toml.push('\n');
+    }
+
+    // sadece cdylib
+    new_toml.push_str("crate-type = [\"cdylib\"]\n");
+
+    fs::write(cargo_manifest, new_toml).unwrap();
+    println!("cargo:warning=Configured hdm_api for C/FFI build");
+}
