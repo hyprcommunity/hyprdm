89040f9708a8e138b454a68dae3e25a1d2070654
Zaman Hüseynli <zamanhuseynli23@gmail.com>
2025-10-02 23:35:31 +0400

    Update rob_build.rs

--- gui-api/hdm_api/rob_build.rs
+++ gui-api/hdm_api/rob_build.rs
@@ -4,18 +4,24 @@ fn main() {
     let cargo_manifest = "Cargo.toml";
     let cargo_toml = fs::read_to_string(cargo_manifest).unwrap();
 
+    let mut lines: Vec<String> = cargo_toml
+        .lines()
+        .filter(|line| !line.trim_start().starts_with("crate-type"))
+        .map(|l| l.to_string())
+        .collect();
+
     let mut new_toml = String::new();
-    for line in cargo_toml.lines() {
-        if line.trim_start().starts_with("crate-type") {
-            continue;
-        }
+    let mut added = false;
+
+    for line in &lines {
         new_toml.push_str(line);
         new_toml.push('\n');
+        if line.trim() == "[lib]" && !added {
+            new_toml.push_str("crate-type = [\"staticlib\"]\n");
+            added = true;
+        }
     }
 
-    // Rust için rlib + staticlib
-    new_toml.push_str("crate-type = [\"rlib\", \"staticlib\"]\n");
-
     fs::write(cargo_manifest, new_toml).unwrap();
-    println!("cargo:warning=Configured hdm_api for Rust build");
+    println!("cargo:warning=Configured hdm_api for Rust build (crate-type = [\"staticlib\"])");
 }
